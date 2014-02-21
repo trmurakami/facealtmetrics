11c3be25298a46ed443aa8ea5f185c04f5590fa2
Tiago Murakami <trmurakami@gmail.com>
2014-02-21 09:53:24 -0300

    Inicial files

---
+++ altmetricsfacebook.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+link="https://graph.facebook.com/fql?q=SELECT%20url,%20normalized_url,%20share_count,%20like_count,%20comment_count,%20total_count,%20commentsbox_count,%20comments_fbid,%20click_count%20FROM%20link_stat%20WHERE%20url=%27"
+linkfim="%27"
+
+echo -e "url\tclick_count\tshare_count\tlike_count\tcomment_count\ttotal_count\tcommentsbox_count\tnormalized_url" > outputfacebook.csv
+
+cat $1 | while read lin ; do curl $link$lin$linkfim |  jq -r '.data[] | .url,.click_count,.share_count,.like_count,.comment_count,.total_count,.commentsbox_count,.normalized_url' | tr "\n" "\t" | cat - newline.txt >> outputfacebook.csv ; done;
